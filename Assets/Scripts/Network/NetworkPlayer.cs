73110fdb2e09691f2b9016b9f74966fc771a969e
Francisco Di Paolo <franciscogermandipaolo@gmail.com>
2022-07-20 15:57:21 -0300

    SpawnerFixed

--- Assets/Scripts/Network/NetworkPlayer.cs
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
-public class NetworkPlayer : MonoBehaviour
+public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
 {
-    // Start is called before the first frame update
+    public static NetworkPlayer Local { get; set; }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Spawned()
     {
-        
+        if (Object.HasInputAuthority)
+        {
+            Local = this;
+
+            Debug.Log("Spawned local player");
+        }
+        else Debug.Log("Spawned remote player");
+    }
+
+    public void PlayerLeft(PlayerRef player)
+    {
+        if (player == Object.InputAuthority)
+            Runner.Despawn(Object);
     }
 }
